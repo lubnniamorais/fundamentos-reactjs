f59aca70be232f20e90065aaef6cf01858974ef3
Lubnnia <lubnnia@gmail.com>
2023-09-01 13:17:57 -0300

    feat: Create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import {Post} from "./Post"
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
 
-      <Post 
-        author="Lubnnia Morais" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, expedita dolores quisquam laborum voluptatibus placeat deserunt repellat eum excepturi possimus facere. Aperiam cumque, voluptatibus ullam molestiae ab pariatur autem quia." 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post 
-        author="Lubni Morais" 
-        content="Um novo post." 
-      />
+        <main>
+          <Post 
+            author="Lubnnia Morais" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, expedita dolores quisquam laborum voluptatibus placeat deserunt repellat eum excepturi possimus facere. Aperiam cumque, voluptatibus ullam molestiae ab pariatur autem quia." 
+          />
+
+          <Post 
+            author="Lubni Morais" 
+            content="Um novo post." 
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar} >
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=50" />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/lubnniamorais.png" />
+
+        <strong>Lubnnia Morais</strong>
+        <span>Web Developer</span>
+
+        <footer>
+          <a href="#">Editar seu perfil</a>
+        </footer>
+      </div>
+    </aside>
+  )
+}
