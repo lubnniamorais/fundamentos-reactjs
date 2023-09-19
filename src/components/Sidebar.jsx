d48bc439cf14fd3d09a58f7c45cc8d9a121ae95c
Lubnnia <lubnnia@gmail.com>
2023-09-19 13:33:15 -0300

    refactor: Update sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from '@phosphor-icons/react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -6,13 +8,16 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=50" />
 
       <div className={styles.profile}>
-        <img src="https://github.com/lubnniamorais.png" />
+        <img className={styles.avatar} src="https://github.com/lubnniamorais.png" />
 
         <strong>Lubnnia Morais</strong>
         <span>Web Developer</span>
 
         <footer>
-          <a href="#">Editar seu perfil</a>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
         </footer>
       </div>
     </aside>
